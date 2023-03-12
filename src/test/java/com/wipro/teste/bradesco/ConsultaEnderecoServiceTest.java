156c9a4628f83fe973fc8fd8524b044ec78f0e6c
RecifeStargazer <carlos.swamy@gmail.com>
2023-03-12 10:40:06 -0300

    testes unitarios com mockito

--- src/test/java/com/wipro/teste/bradesco/ConsultaEnderecoServiceTest.java
+++ src/test/java/com/wipro/teste/bradesco/ConsultaEnderecoServiceTest.java
@@ -1,5 +1,5 @@
 package com.wipro.teste.bradesco;
-
+import com.wipro.teste.bradesco.pojo.ViaCep;
 import com.wipro.teste.bradesco.restClient.RestTemplateClient;
 import com.wipro.teste.bradesco.services.ConsultaEnderecoService;
 import com.wipro.teste.bradesco.services.ConsultaEnderecoServiceImpl;
@@ -7,35 +7,50 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
+
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ConsultaEnderecoServiceTest {
 
     private static ConsultaEnderecoService consultaEnderecoService;
 
+    private static RestTemplateClient mockedRestTemplateClient;
+
     @BeforeAll
     static void setup() {
-        consultaEnderecoService = new ConsultaEnderecoServiceImpl(new RestTemplateClient(new RestTemplate()));
+        mockedRestTemplateClient = Mockito.mock(RestTemplateClient.class);
+        consultaEnderecoService = new ConsultaEnderecoServiceImpl(mockedRestTemplateClient);
     }
 
     @Test
     public void deveConsultarCepCorretoComHifen(){
         var cep = "52051-280";
+        var viaCep =  new ViaCep();
+        viaCep.setCep(cep);
+        viaCep.setUf("PE");
+        when(mockedRestTemplateClient.getEnderecoCep(cep)).thenReturn(viaCep);
         Assertions.assertEquals(cep, consultaEnderecoService.getEnderecoPorCep(cep).getCep());
     }
 
     @Test
     public void deveConsultarCepCorretoSemHifen(){
         var cep = "52051280";
-        Assertions.assertEquals(cep, consultaEnderecoService.getEnderecoPorCep(cep).getCep().replace("-",""));
+        var viaCep =  new ViaCep();
+        viaCep.setCep(cep);
+        viaCep.setUf("PE");
+        when(mockedRestTemplateClient.getEnderecoCep(cep)).thenReturn(viaCep);
+        Assertions.assertEquals(cep, consultaEnderecoService.getEnderecoPorCep(cep).getCep());
     }
 
     @Test
     public void deveConsultarCepErrado(){
         var cep = "11111111";
+        when(mockedRestTemplateClient.getEnderecoCep(cep)).thenThrow(new HttpClientErrorException(HttpStatus.NOT_FOUND));
         Assertions.assertThrows(HttpClientErrorException.class, () -> consultaEnderecoService.getEnderecoPorCep(cep));
     }
 }
--- src/test/java/com/wipro/teste/bradesco/RestTemplateClientTest.java
+++ src/test/java/com/wipro/teste/bradesco/RestTemplateClientTest.java
@@ -1,40 +1,54 @@
 package com.wipro.teste.bradesco;
 
-
+import com.wipro.teste.bradesco.pojo.ViaCep;
 import com.wipro.teste.bradesco.restClient.RestTemplateClient;
-import com.wipro.teste.bradesco.services.ConsultaEnderecoServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.web.client.RestTemplate;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RestTemplateClientTest {
 
     private static RestTemplateClient restTemplateClient;
 
+    private static RestTemplate mockedRestTemplate;
+
+
     @BeforeAll
     static void setup() {
-        restTemplateClient = new RestTemplateClient(new RestTemplate());
+        mockedRestTemplate = Mockito.mock(RestTemplate.class);
+        restTemplateClient = new RestTemplateClient(mockedRestTemplate);
     }
 
     @Test
     public void deveConsultarCepCorretoComHifen(){
         var cep = "52051-280";
+        ViaCep viaCep = new ViaCep();
+        viaCep.setCep(cep);
+        when(mockedRestTemplate.getForObject("https://viacep.com.br/ws/52051-280/json/", ViaCep.class)).thenReturn(viaCep);
         Assertions.assertEquals(cep, restTemplateClient.getEnderecoCep(cep).getCep());
     }
 
     @Test
     public void deveConsultarCepCorretoSemHifen(){
         var cep = "52051280";
-        Assertions.assertEquals(cep, restTemplateClient.getEnderecoCep(cep).getCep().replace("-",""));
+        ViaCep viaCep = new ViaCep();
+        viaCep.setCep(cep);
+        when(mockedRestTemplate.getForObject("https://viacep.com.br/ws/52051280/json/", ViaCep.class)).thenReturn(viaCep);
+        Assertions.assertEquals(cep,restTemplateClient.getEnderecoCep(cep).getCep());
     }
 
     @Test
     public void deveConsultarCepErrado(){
-       var cep = "11111111";
-       Assertions.assertEquals(restTemplateClient.getEnderecoCep(cep).getErro(), true);
+        var cep = "11111111";
+        ViaCep viaCep = new ViaCep();
+        viaCep.setErro(true);
+        when(mockedRestTemplate.getForObject("https://viacep.com.br/ws/11111111/json/", ViaCep.class)).thenReturn(viaCep);
+        Assertions.assertEquals(restTemplateClient.getEnderecoCep(cep).getErro(), true);
     }
 }
