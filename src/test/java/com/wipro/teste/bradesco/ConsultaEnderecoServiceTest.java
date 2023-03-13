55b0cbe1f5a7f6e0af0a2b27813415d94a0c00cc
RecifeStargazer <carlos.swamy@gmail.com>
2023-03-12 21:15:44 -0300

    finnaly make it, junit5/jupyter and cucumber together on maven

--- src/test/java/com/wipro/teste/bradesco/ConsultaEnderecoServiceTest.java
+++ src/test/java/com/wipro/teste/bradesco/ConsultaEnderecoServiceTest.java
@@ -6,15 +6,17 @@ import com.wipro.teste.bradesco.services.ConsultaEnderecoServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class ConsultaEnderecoServiceTest {
 
     private static ConsultaEnderecoService consultaEnderecoService;
--- src/test/java/com/wipro/teste/bradesco/RestTemplateClientTest.java
+++ src/test/java/com/wipro/teste/bradesco/RestTemplateClientTest.java
@@ -5,13 +5,15 @@ import com.wipro.teste.bradesco.restClient.RestTemplateClient;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.RestTemplate;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class RestTemplateClientTest {
 
     private static RestTemplateClient restTemplateClient;
